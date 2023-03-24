c7d3b92fb19acaadc4e52614dc598bcb82404403
Mahboub99 <ahmed.mahboub99@gmail.com>
2023-03-24 18:17:03 +0200

    bank modifications

--- src/components/RegisterCompletedElement/RegisterCompletedElement.jsx
+++ src/components/RegisterCompletedElement/RegisterCompletedElement.jsx
@@ -7,15 +7,13 @@ import './RegisterCompletedElement.css';
 function RegisterCompletedElement(props) {
     return (
         <div className="registerCompletedElement">
-
             <div className="registerCompletedElement__container">
                 <div className='registerCompletedElement__container2'>
                     <img className="registerCompletedElement__container--image" src={props.img} alt="completed"/>
                 </div>
-
                 <div className="registerCompletedElement__text">
                     <p className='registerCompletedElement__text--hover' >{props.hoverText}</p>
-                    <p className='registerCompletedElement__text--main'>{props.text}</p>
+                    <p className='registerCompletedElement__text--main'>&nbsp;{props.text}</p>
                 </div>
             </div>
         </div>
